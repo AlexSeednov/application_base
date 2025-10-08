88d010c897d5cc1497993ffea0b334357da71008
Seednov 'Alex' Alexander <seednov.alexander@gmail.com>
2025-10-08 18:20:30 +0300

    auth check possible fix

--- lib/presentation/navigation/guard/authentication_guard.dart
+++ lib/presentation/navigation/guard/authentication_guard.dart
@@ -1,6 +1,5 @@
 import 'package:application_base/core/service/logger_service.dart';
 import 'package:application_base/core/service/service_locator.dart';
-import 'package:application_base/presentation/navigation/navigation_service.dart';
 import 'package:application_base/presentation/view_model/access_vm.dart';
 import 'package:auto_route/auto_route.dart';
 
@@ -20,7 +19,7 @@ class AuthenticationGuard implements AutoRouteGuard {
     } else {
       /// Need to sign in or sign up before
       logInfo(info: 'Try to open ${resolver.routeName} without access');
-      await pushScreen(route: authorizationRoute);
+      await resolver.redirectUntil(authorizationRoute);
       resolver.next(getIt<AccessVM>().isGranted);
     }
   }
